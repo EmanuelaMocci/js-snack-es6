312245adfc087b3fa9d5b6dd2f71c522762c7b08
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-10 09:54:23 +0200

    Push snack-1

---
+++ snack-1/script.js
@@ -0,0 +1,35 @@
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+const bici = [
+
+    {
+        'nome': 'bike1',
+        'peso': 13
+    },
+
+    {
+        'nome': 'bike2',
+        'peso': 10
+    },
+
+    {
+        'nome': 'bike3',
+        'peso': 15
+    }
+];
+   
+
+// Stampare a schermo la bici con peso minore
+let pesoMinore = bici[0];
+
+for (let i = 0; i < bici.length; i++) {
+    if (bici[i].peso < pesoMinore.peso){
+        pesoMinore = bici[i];
+    }
+}
+
+
+
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+const {nome, peso} = bici;
+
+console.log(`La bici più leggera è ${pesoMinore.nome} e pesa ${pesoMinore.peso}kg`)
